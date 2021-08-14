b5f20c3733a974528eb615c277e0c3a36c71b5e5
azizamari <azizamari2003@gmail.com>
2021-08-14 05:30:49 +0100

    Draw program logo and set color to yellow

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+
+def print_logo():
+    os.system('color E')
+    print("""      
+       _                        
+       \`*-.                    
+        )  _`-.                 
+       .  : `. .                
+       : _   '  \               
+       ; *` _.   `*-._          
+       `-.-'          `-.       
+         ;       `       `.        Alaa Utilty Tool
+         :.       .        \       By @azizamari
+         . \  .   :   .-'   .   
+         '  `+.;  ;  '      :   
+         :  '  |    ;       ;-. 
+         ; '   : :`-:     _.`* ;
+      .*' /  .*' ; .*`- +'  `*' 
+      `*-*   `*-*  `*-*'
+    """)
+
+# MAIN PROGRAM
+print_logo()
